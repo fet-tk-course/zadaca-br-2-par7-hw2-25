a358e13b36e4ccb0b44d841e75e2ba603f4d6032
Lamija Altumbabic <lamija.altumbabic@fet.ba>
2026-05-09 22:15:49 +0200

    Uvezan restaurant_router

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ from contextlib import asynccontextmanager
 
 from database import create_db_and_tables
 from routes_b import router as food_router
-
 from routes_a import router as restaurant_router
 
 @asynccontextmanager
@@ -22,3 +21,8 @@ app = FastAPI(
 @app.get("/")
 def read_root():
     return {"message": "Zadaća 2 - REST API"}
+
+
+
+
+app.include_router(restaurant_router)
