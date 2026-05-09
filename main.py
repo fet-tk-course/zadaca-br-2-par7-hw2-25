dc7f4d7f879d2f94ca01500ff5c3a44af0d5b915
hedijasisic <sisichedija23@gmail.com>
2026-05-09 16:31:28 +0200

    Importovan router iz routes_b i uključene sve foods rute

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 from contextlib import asynccontextmanager
 
 from database import create_db_and_tables
+from routes_b import router as food_router
 
 
 @asynccontextmanager
@@ -20,3 +21,5 @@ app = FastAPI(
 @app.get("/")
 def read_root():
     return {"message": "Zadaća 2 - REST API"}
+
+app.include_router(food_router)
