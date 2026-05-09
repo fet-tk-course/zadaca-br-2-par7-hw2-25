56ef3a81c8d9e41740f7d9231d11300b5a5f3033
hedijasisic <sisichedija23@gmail.com>
2026-05-09 15:34:14 +0200

    Napravljen metod za dohvatanje svih jela

--- routes_b.py
+++ routes_b.py
@@ -5,3 +5,8 @@ from database import get_session
 from models_b import Food, FoodCreate, FoodUpdate
 
 router = APIRouter(prefix="/foods", tags=["Foods"])
+
+@router.get("/")
+def get_all_foods(session: Session = Depends(get_session)):
+    foods = session.exec(select(Food)).all()
+    return foods
