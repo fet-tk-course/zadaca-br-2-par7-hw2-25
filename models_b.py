fb07ad63dc85625e55622f8b1c5ce217ae821a4a
hedijasisic <sisichedija23@gmail.com>
2026-05-14 15:39:03 +0200

    Napisani validatori za provjeru imena i cijene

--- models_b.py
+++ models_b.py
@@ -1,5 +1,6 @@
 from sqlmodel import SQLModel, Field
 from typing import Optional
+from pydantic import field_validator
 
 class Food(SQLModel, table=True):
     id: Optional[int] = Field(default=None, primary_key=True)
@@ -28,3 +29,24 @@ class FoodUpdate(SQLModel):
     calories: Optional[int] = None
     available: Optional[bool] = None
     description: Optional[str] = None
+
+    @field_validator("name")
+    @classmethod 
+    def naziv_ne_smije_biti_prazan(cls, value):
+        if not value.strip():
+            raise ValueError('Name ne smije biti prazan string')
+        
+    @classmethod
+    def naziv_mora_imatt_min_duzinu(cls, value):
+        if len(value.strip()) < 2:
+            raise ValueError('Name mora imati minimalnu dužinu od 2 znaka')
+        return value.strip()
+  
+    @field_validator('price')
+    @classmethod
+    def price_mora_biti_pozitivan(cls,v):
+        if v <= 0:
+            raise ValueError('Price mora biti veća od nule')
+        return v
+
+   
