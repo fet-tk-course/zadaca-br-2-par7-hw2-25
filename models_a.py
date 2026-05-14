ea31a01adc5da46cb204cfa958a66fe588fb9f42
Lamija Altumbabic <lamija.altumbabic@fet.ba>
2026-05-14 15:35:38 +0200

    Zadatak 1a - Dodati validatori

--- models_a.py
+++ models_a.py
@@ -1,5 +1,7 @@
 from sqlmodel import SQLModel, Field
 from typing import Optional
+from pydantic import field_validator
+
 
 class Restaurant(SQLModel, table=True):
     id: Optional[int] = Field(default=None, primary_key=True)
@@ -26,3 +28,22 @@ class RestaurantUpdate(SQLModel):
     rating: Optional[int] = None
     is_open: Optional[bool] = None
     address: Optional[str] = None
+
+
+@field_validator('naziv')
+@classmethod
+def naziv_ne_smije_biti_prazan(cls, v):
+    if not v.strip():
+        raise ValueError('Naziv restorana ne smije biti prazan')
+    return v.strip() 
+
+@field_validator('ocjena')
+@classmethod
+def raspon_ocjene(cls, v):
+    if v < 1 or v > 5:
+        raise ValueError('Ocjena mora biti u rasponu od 1 do 5')
+    return v
+
+
+
+
